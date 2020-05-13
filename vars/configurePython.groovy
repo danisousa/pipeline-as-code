59019777b178de6538bb5dbe972ed5a50d197f9d
danisousa <47631655+danisousa@users.noreply.github.com>
2020-05-13 13:53:01 +0200

    Update configurePython.groovy

--- vars/configurePython.groovy
+++ vars/configurePython.groovy
@@ -1,6 +1,6 @@
 def call() {
-	sh(returnStdout: false, label: "[Python Agent]: Updating references..", script: "apt-get update")
-	sh(returnStdout: false, label: "[Python Agent]: Installing git..", script: "apt-get install -y --no-install-recommends git")
+	sh(returnStdout: true, label: "[Python Agent]: Updating references..", script: "apt-get update")
+	sh(returnStdout: true, label: "[Python Agent]: Installing git..", script: "apt-get install -y --no-install-recommends git")
 	sh(returnStdout: true, label: "[Python Agent]: Cloning python utils..", script: "git clone https://github.com/danisousa/python-utils.git")
 	sh(returnStdout: true, label: "[Python Agent]: Installing python dependencies...", script: "pip install -r python-utils/requirements.txt")
 }
