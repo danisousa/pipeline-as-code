4baace0a37520b3d6d5d1743238895a40d5327c0
Daniel <sousadgsou@gmail.com>
2020-05-14 12:06:43 +0200

    fix script

--- vars/checkDependencies.groovy
+++ vars/checkDependencies.groovy
@@ -1,4 +1,4 @@
 def call() {
-	def deps = sh(returnStdout: true, label: "[Python Agent]: Checking dependencies..", script: "")
+	def deps = sh(returnStdout: true, label: "[Python Agent]: Checking dependencies..", script: "check_deps.py")
 	println "${deps}"
 }
