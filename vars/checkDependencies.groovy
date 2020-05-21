7856fafc1f21d87290dd382a17e9bbd05fa988d1
Daniel <sousadgsou@gmail.com>
2020-05-21 10:10:36 +0200

    fix print

--- vars/checkDependencies.groovy
+++ vars/checkDependencies.groovy
@@ -2,7 +2,6 @@ def call() {
 	def deps = sh(returnStdout: true, label: "[Python Agent]: Checking dependencies..", script: "python python-utils/check_deps.py")
 	if(deps.length() > 0){
 		currentBuild.result='UNSTABLE'
-		println "DEPENDENCIES NOT VALIDATED:"
 		println "${deps}"
 	}else{
 		println "All dependencies are validated"
